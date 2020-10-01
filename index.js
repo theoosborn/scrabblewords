4d01a52a403500aa56b630453ab0394cf2cc756d
Theo Osborn <22201749+theoosborn@users.noreply.github.com>
2020-10-01 19:26:16 +0100

    Capitalised letters

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ var numUsers = 0;
 // Game functions
 var wordset = [];
 
-alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0"];
+alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0"];
 numLetters = [9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1, 2, 1, 2];
 function addLetters(item, index) {
 	var repeatLetter = item;
