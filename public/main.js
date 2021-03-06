8dc2e4490247ac7751ea791b994a378b6c67826c
Theo Osborn <22201749+theoosborn@users.noreply.github.com>
2021-03-06 12:31:07 +0000

    Converts tabs to spaces

--- public/main.js
+++ public/main.js
@@ -1,137 +1,137 @@
 $(function() {
 
-	// let's init some variables
-	var usernameInput = $('.usernameInput');
-	var usernameButton = $('.usernameButton');
-	var initScrabbleArray = $('.initScrabbleArray');
-	var getNextLetter = $('.getNextLetter');
-	var resetButton = $('.resetButton');  
+    // let's init some variables
+    var usernameInput = $('.usernameInput');
+    var usernameButton = $('.usernameButton');
+    var initScrabbleArray = $('.initScrabbleArray');
+    var getNextLetter = $('.getNextLetter');
+    var resetButton = $('.resetButton');  
 
-	var loginPage = $('.login.page');
-	var gamePage = $('.game.page');
-	var messages = $('.messages');
+    var loginPage = $('.login.page');
+    var gamePage = $('.game.page');
+    var messages = $('.messages');
 
-	// Prompt for setting a username
-	var username;
-	var connected = false;
-	var currentInput = usernameInput.focus();
+    // Prompt for setting a username
+    var username;
+    var connected = false;
+    var currentInput = usernameInput.focus();
 
-	var socket = io();
+    var socket = io();
 
-	const sendMsg = (data, type) => {
-		messages.prepend("<p class='message " + type + "'>" + data + "</p>");
-	};
+    const sendMsg = (data, type) => {
+        messages.prepend("<p class='message " + type + "'>" + data + "</p>");
+    };
 
-	const addParticipantsMessage = (data) => {
-		var message = '';
-		if (data.numUsers === 1) {
-			message += "There's 1 participant.";
-		} else {
-			message += "There are " + data.numUsers + " participants.";
-		}
-		sendMsg(message);
-	};
+    const addParticipantsMessage = (data) => {
+        var message = '';
+        if (data.numUsers === 1) {
+            message += "There's 1 participant.";
+        } else {
+            message += "There are " + data.numUsers + " participants.";
+        }
+        sendMsg(message);
+    };
 
-	// sets username for client
+    // sets username for client
 
-	const setUsername = () => {
-		username = cleanInput(usernameInput.val().trim());
+    const setUsername = () => {
+        username = cleanInput(usernameInput.val().trim());
 
-		// if it's valid...
-		if (username) {
-			loginPage.fadeOut();
-			gamePage.fadeIn();
-			loginPage.off('click');
+        // if it's valid...
+        if (username) {
+            loginPage.fadeOut();
+            gamePage.fadeIn();
+            loginPage.off('click');
 
-			// tell the server the username
-			socket.emit('add user', username);
-		}
-	};
+            // tell the server the username
+            socket.emit('add user', username);
+        }
+    };
 
-	// Prevents input from having injected markup
-	const cleanInput = (input) => {
-		return $('<div/>').text(input).html();
-	};
+    // Prevents input from having injected markup
+    const cleanInput = (input) => {
+        return $('<div/>').text(input).html();
+    };
 
-	// Scrabble game code starts here
+    // Scrabble game code starts here
 
 
-	// Click events
+    // Click events
 
-	loginPage.click(() => {
-		currentInput.focus();
-	});
+    loginPage.click(() => {
+        currentInput.focus();
+    });
 
-	usernameButton.click(() => {
-		if (!username) {
-			setUsername();
-		}
-	});
+    usernameButton.click(() => {
+        if (!username) {
+            setUsername();
+        }
+    });
 
-	initScrabbleArray.click(() => {
-		/* 
-		old code
-		if (wordset.length == 0){
-			numLetters.forEach(addLetters);
-			// Shuffle the array so you can't predict which letter you'll get
-			shuffleArray(wordset);
-			console.log(wordset.toString());
-		} else {
-			console.log("Wordset already exists");
-		}
-		*/
-		socket.emit('wordset init');
-	});
+    initScrabbleArray.click(() => {
+        /* 
+        old code
+        if (wordset.length == 0){
+            numLetters.forEach(addLetters);
+            // Shuffle the array so you can't predict which letter you'll get
+            shuffleArray(wordset);
+            console.log(wordset.toString());
+        } else {
+            console.log("Wordset already exists");
+        }
+        */
+        socket.emit('wordset init');
+    });
 
-	getNextLetter.click(() => {
-		socket.emit('letter picked');
-	});
+    getNextLetter.click(() => {
+        socket.emit('letter picked');
+    });
 
-	resetButton.click(() => {
-		socket.emit('reset');
-	});
+    resetButton.click(() => {
+        socket.emit('reset');
+    });
 
-	// Socket events
+    // Socket events
 
-	socket.on('login', (data) => {
-		connected = true;
-		addParticipantsMessage(data);
-	});
+    socket.on('login', (data) => {
+        connected = true;
+        addParticipantsMessage(data);
+    });
 
-	socket.on('user has joined', (data) => {
-		sendMsg(data.username + ' joined.', "sys");
-		addParticipantsMessage(data);
-	});
+    socket.on('user has joined', (data) => {
+        sendMsg(data.username + ' joined.', "sys");
+        addParticipantsMessage(data);
+    });
 
-	socket.on('send message', (data) => {
-		sendMsg(data.message);
-	});
+    socket.on('send message', (data) => {
+        sendMsg(data.message);
+    });
 
-	socket.on('user has left', (data) => {
-		sendMsg(data.username + ' left.', "sys");
-		addParticipantsMessage(data);
-	});
+    socket.on('user has left', (data) => {
+        sendMsg(data.username + ' left.', "sys");
+        addParticipantsMessage(data);
+    });
 
-	socket.on('disconnect', () => {
-		sendMsg('You have been disconnected.', "sys");
-	});
+    socket.on('disconnect', () => {
+        sendMsg('You have been disconnected.', "sys");
+    });
 
-	socket.on('reconnect', () => {
-		sendMsg('You have been reconnected.', "sys");
-		if (username) {
-			socket.emit('add user', username);
-		}
-	});
+    socket.on('reconnect', () => {
+        sendMsg('You have been reconnected.', "sys");
+        if (username) {
+            socket.emit('add user', username);
+        }
+    });
 
-	socket.on('reconnect error', () => {
-		sendMsg('Attempt to reconnect has failed.', "sys");
-	});
+    socket.on('reconnect error', () => {
+        sendMsg('Attempt to reconnect has failed.', "sys");
+    });
 
-	socket.on('letter picked', (data) => {
-		if (data.username == username){
-			sendMsg("You have picked up <span class='tile'>" + data.message + "</span>.");
-		} else {
-			sendMsg(data.username + " has picked up a letter.");
-		}
-	});
+    socket.on('letter picked', (data) => {
+        if (data.username == username){
+            sendMsg("You have picked up <span class='tile'>" + data.message + "</span>.");
+        } else {
+            sendMsg(data.username + " has picked up a letter.");
+        }
+    });
 });
