eb261f92ac8635a6116f8e48c31bbe478a768d29
Theo Osborn <22201749+theoosborn@users.noreply.github.com>
2021-03-06 10:32:55 +0000

    Converts node file indentation to spaces

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var io = require('socket.io')(server);
 var port = process.env.PORT || 3001;
 
 server.listen(port, () => {
-	console.log('Server listening at port %d', port);
+    console.log('Server listening at port %d', port);
 });
 
 // Routing
@@ -23,118 +23,118 @@ var wordset = [];
 const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0"];
 const numLetters = [9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1, 2, 1, 2];
 function addLetters(item, index) {
-	var repeatLetter = item;
-	// Add correct number of each letter for a scrabble game
-	while (repeatLetter > 0) {
-			wordset.push(alphabet[index]);
-		repeatLetter--;
+    var repeatLetter = item;
+    // Add correct number of each letter for a scrabble game
+    while (repeatLetter > 0) {
+            wordset.push(alphabet[index]);
+        repeatLetter--;
   }
 }
 
 // Durstenfeld shuffle the array
 function shuffleArray(array) {
-	for (let i = array.length - 1; i > 0; i--) {
-		const j = Math.floor(Math.random() * (i + 1));
-		[array[i], array[j]] = [array[j], array[i]];
-	}
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
 }
-  
+
 
 
 io.on('connection', (socket) => {
 
-	// Some game code
-	const pickLetter = () => {
-		var letterPicked = wordset.pop();
-		if (letterPicked == "0") {
-			letterPicked = "&#10240;";
-		}
-		return letterPicked;
-		/* old code
-		$messages.prepend("<p class='message'>Either the wordset array has not been initialised or you've run out of letters!</p>")
-		console.log("Either the wordset array has not been initialised or you've run out of letters!");
-		*/
-	}; 
+    // Some game code
+    const pickLetter = () => {
+        var letterPicked = wordset.pop();
+        if (letterPicked == "0") {
+            letterPicked = "&#10240;";
+        }
+        return letterPicked;
+        /* old code
+        $messages.prepend("<p class='message'>Either the wordset array has not been initialised or you've run out of letters!</p>")
+        console.log("Either the wordset array has not been initialised or you've run out of letters!");
+        */
+    }; 
 
 
-	var addedUser = false;
+    var addedUser = false;
 
-	// Server functions
+    // Server functions
 
-	socket.on('new message', (data) => {
-		socket.broadcast.emit('new message', {
-			username: socket.username,
-			message: data
-		});
-	});
+    socket.on('new message', (data) => {
+        socket.broadcast.emit('new message', {
+            username: socket.username,
+            message: data
+        });
+    });
 
-	socket.on('add user', (username) => {
-		if (addedUser) return;
+    socket.on('add user', (username) => {
+        if (addedUser) return;
 
-		// store the username in the socket session for this client
-		socket.username = username;
-		++numUsers;
-		addedUser = true;
-		socket.emit('login', {
-			numUsers: numUsers
-		});
-		// echo globally that a person has connected
-		socket.broadcast.emit('user has joined', {
-			username: socket.username,
-			numUsers: numUsers
-		});
-	});
+        // store the username in the socket session for this client
+        socket.username = username;
+        ++numUsers;
+        addedUser = true;
+        socket.emit('login', {
+            numUsers: numUsers
+        });
+        // echo globally that a person has connected
+        socket.broadcast.emit('user has joined', {
+            username: socket.username,
+            numUsers: numUsers
+        });
+    });
 
-	socket.on('disconnect', () => {
-		if (addedUser) {
-			--numUsers;
+    socket.on('disconnect', () => {
+        if (addedUser) {
+            --numUsers;
 
-			// echo globally that this client has left
-			socket.broadcast.emit('user has left', {
-				username: socket.username,
-				numUsers: numUsers
-			});
-		}
-	});
+            // echo globally that this client has left
+            socket.broadcast.emit('user has left', {
+                username: socket.username,
+                numUsers: numUsers
+            });
+        }
+    });
 
-	socket.on('wordset init', () => {
-		if (wordset.length == 0) {
-			numLetters.forEach(addLetters);
-			// Shuffle the array
-			shuffleArray(wordset);
-			io.emit('send message', {
-				username: socket.username,
-				message: socket.username + " has initialised the wordset"
-			});
-		} else {
-			io.emit('send message', {
-				username: socket.username,
-				message: socket.username + " attempted to initialise the wordset but it already exists."
-			});
-		}
-	});
+    socket.on('wordset init', () => {
+        if (wordset.length == 0) {
+            numLetters.forEach(addLetters);
+            // Shuffle the array
+            shuffleArray(wordset);
+            io.emit('send message', {
+                username: socket.username,
+                message: socket.username + " has initialised the wordset"
+            });
+        } else {
+            io.emit('send message', {
+                username: socket.username,
+                message: socket.username + " attempted to initialise the wordset but it already exists."
+            });
+        }
+    });
 
-	socket.on('letter picked', () => {
-		if (wordset.length != 0) {
-			var letterPicked = pickLetter();
-			io.emit('letter picked', {
-				username: socket.username,
-				message: letterPicked
-			});
-		} else {
-			io.emit('send message', {
-				username: socket.username,
-				message: socket.username + " attempted to get a letter but either the wordset array has not been initialised or you've run out of letters!"
-			});
-		}
+    socket.on('letter picked', () => {
+        if (wordset.length != 0) {
+            var letterPicked = pickLetter();
+            io.emit('letter picked', {
+                username: socket.username,
+                message: letterPicked
+            });
+        } else {
+            io.emit('send message', {
+                username: socket.username,
+                message: socket.username + " attempted to get a letter but either the wordset array has not been initialised or you've run out of letters!"
+            });
+        }
 
-	});
+    });
 
-	socket.on('reset', () => {
-		wordset = [];
-		io.emit('send message', {
-			username: socket.username,
-			message: socket.username + " has reset the game."
-		});
-	});
+    socket.on('reset', () => {
+        wordset = [];
+        io.emit('send message', {
+            username: socket.username,
+            message: socket.username + " has reset the game."
+        });
+    });
 });
