704ee6b611d61ee6fa2a4af8f714e2b515da6748
Theo Osborn <22201749+theoosborn@users.noreply.github.com>
2021-04-03 18:10:51 +0100

    Logs any socket event to console

--- frontend/src/plugins/socketio.js
+++ frontend/src/plugins/socketio.js
@@ -4,4 +4,8 @@ const URL = "http://localhost:3001";
 
 const socket = io(URL, { autoConnect: false });
 
+socket.onAny((event, ...args) => {
+	console.log(event, args);
+});
+
 export default socket;
