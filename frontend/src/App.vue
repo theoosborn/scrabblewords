9f031ede99a972b73244d44c7a9698b28b206917
Theo Osborn <22201749+theoosborn@users.noreply.github.com>
2021-04-03 16:17:43 +0100

    Restructures vue components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,23 +3,41 @@
     <header class="header">
         <h1>crabble</h1>
     </header>
-    <ScrabbleGame />
+    <SelectUsername :error="connectError" v-if="!isLoggedIn" @login-request="onUsernameSelection" />
+    <div v-else>
+      <li v-for="message in serverOutput" v-bind:key="message.received">{{ message }}</li>
+    </div>
   </div>
 </template>
 
 <script>
-import ScrabbleGame from './components/ScrabbleGame.vue'
+import SelectUsername from "./components/SelectUsername.vue";
+import socket from "./plugins/socketio.js";
 
 export default {
   name: 'App',
   components: {
-    ScrabbleGame
+    SelectUsername
   },
   data: function() {
     return {
-      test: true
+      test: true,
+      isLoggedIn: false,
+      connectError: ""
     }
   },
+  methods: {
+    onUsernameSelection(username) {
+      this.loggedIn = true;
+      socket.auth = { username };
+      socket.connect();
+    }
+  },
+  created() {
+    socket.on("connect_error", (err) => {
+      this.connectError = err;
+    });
+  }
 }
 </script>
 
---
+++ frontend/src/components/SelectUsername.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <label>Please enter your username: 
+            <input v-model="username" type="text" id="name" required>
+        </label>
+        <p v-if="error" class="error">{{ error }}</p>
+        <button :disabled="!isValid" @click="login()">Login</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SelectUsername",
+        data () {
+            return {
+                username: ""
+            }
+        },
+        props: {
+            error: String
+        },
+        methods: {
+            submit() {
+                this.$emit("submit", this.username);
+            }
+        },
+        computed: {
+            isValid() {
+                return this.username.length > 0;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    p.error {
+        color: red;
+    }
+</style>
