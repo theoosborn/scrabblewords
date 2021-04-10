84bce6704545481c57c483b3ac735b8ecb267105
Theo Osborn <22201749+theoosborn@users.noreply.github.com>
2021-04-10 13:49:37 +0100

    Replaces blank key with unicode character

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ httpServer.listen(3001, () => {
 var randomisedLetters = []; // The letters available to players in the game. They have been randomised.
 
 const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", 
-                  "U", "V", "W", "X", "Y", "Z", "0"];
+                  "U", "V", "W", "X", "Y", "Z", " "];
 const numLetters = [9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1, 2, 1, 2];
 
 function addLetter(item, index) {
@@ -37,9 +37,6 @@ function shuffleArray(array) {
 
 function pickLetter() {
     let letterPicked = randomisedLetters.pop();
-    if (letterPicked == "0") {
-        letterPicked = "&#10240;";
-    }
     return letterPicked;
 }
 
