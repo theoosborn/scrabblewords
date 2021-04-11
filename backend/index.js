14426f281a93565dad4cc13ae50ab471532261c8
Theo Osborn <22201749+theoosborn@users.noreply.github.com>
2021-04-11 16:22:50 +0100

    Formats index.js

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,14 @@
 // Setup express server
 const httpServer = require("http").createServer();
-const io = require('socket.io')(httpServer, {
+const io = require("socket.io")(httpServer, {
     cors: {
         origin: "http://localhost:8080",
-        methods: ["GET", "POST"]
-    }
+        methods: ["GET", "POST"],
+    },
 });
 
 httpServer.listen(3001, () => {
-    console.log('Server listening at port 3001');
+    console.log("Server listening at port 3001");
 });
 
 // Scrabble room
@@ -16,9 +16,64 @@ httpServer.listen(3001, () => {
 // Game functions
 var randomisedLetters = []; // The letters available to players in the game. They have been randomised.
 
-const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", 
-                  "U", "V", "W", "X", "Y", "Z", " "];
-const numLetters = [9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1, 2, 1, 2];
+const alphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    " ",
+];
+const numLetters = [
+    9,
+    2,
+    2,
+    4,
+    12,
+    2,
+    3,
+    2,
+    9,
+    1,
+    1,
+    4,
+    2,
+    6,
+    8,
+    2,
+    1,
+    6,
+    4,
+    6,
+    4,
+    2,
+    2,
+    1,
+    2,
+    1,
+    2,
+];
 
 function addLetter(item, index) {
     // Add correct number of each letter for a scrabble game
@@ -58,7 +113,6 @@ io.use((socket, next) => {
     next();
 });
 
-
 io.on("connection", (socket) => {
     const users = [];
     for (let [id, socket] of io.of("/").sockets) {
@@ -70,13 +124,12 @@ io.on("connection", (socket) => {
     socket.emit("users", users);
     socket.broadcast.emit("user connected", {
         userID: socket.id,
-        username: socket.username
+        username: socket.username,
     });
 
     // Server functions
 
-    socket.on('disconnect', () => {
-
+    socket.on("disconnect", () => {
         // Reset the game if everybody has left.
         if (io.of("/").sockets.size === 0) {
             randomisedLetters = [];
@@ -85,37 +138,34 @@ io.on("connection", (socket) => {
         // echo globally that this client has left
         socket.broadcast.emit("user disconnected", {
             userID: socket.id,
-            username: socket.username
+            username: socket.username,
         });
     });
 
-    socket.on('initialise letterset', () => {
+    socket.on("initialise letterset", () => {
         if (randomisedLetters.length === 0) {
-
             numLetters.forEach(addLetter);
             shuffleArray(randomisedLetters);
 
-            io.emit('letterset initialised', socket.username);
-
+            io.emit("letterset initialised", socket.username);
         } else {
-            io.emit('letterset already exists', socket.username);
+            io.emit("letterset already exists", socket.username);
         }
     });
 
-    socket.on('get letter', () => {
+    socket.on("get letter", () => {
         if (randomisedLetters.length !== 0) {
             // Emit message to client who picked letter.
-            socket.emit('picked letter', pickLetter());
+            socket.emit("picked letter", pickLetter());
             // Emit message to all other than client.
-            socket.broadcast.emit('someone picked letter', socket.username);
+            socket.broadcast.emit("someone picked letter", socket.username);
         } else {
-            io.emit('letterset not initialised', socket.username);
+            io.emit("letterset not initialised", socket.username);
         }
-
     });
 
-    socket.on('reset', () => {
+    socket.on("reset", () => {
         randomisedLetters = [];
-        io.emit('reset', socket.username);
+        io.emit("reset", socket.username);
     });
 });
