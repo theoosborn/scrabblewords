944fdafae7bc99071f9af58827eb6dcece36df91
Theo Osborn <22201749+theoosborn@users.noreply.github.com>
2021-09-06 20:41:26 +0100

    Disables logging in production

--- frontend/src/plugins/socketio.js
+++ frontend/src/plugins/socketio.js
@@ -4,8 +4,10 @@ const URL = 'http://localhost:3001';
 
 const socket = io(URL, { autoConnect: false });
 
-socket.onAny((event, ...args) => {
-  console.log(event, args);
-});
+if (process.env.NODE_ENV !== "production") {
+  socket.onAny((event, ...args) => {
+    console.log(event, args);
+  });
+}
 
 export default socket;
